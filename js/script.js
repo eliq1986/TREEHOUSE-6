27b9c6b8da1f6eac3e82e5c98a3f3246b71e627e
Elijah Quesada <this_boy10@yahoo.com>
2018-03-12 15:51:27 -0700

    Final Commit
    Finished project added last CSS styles.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const videoPlayer = document.querySelector(".mejs__player"); //Grabs video element
+const spanParagraphs = document.querySelectorAll("span"); //Grabs all span elements aka collection of span tags
+
+
+
+ videoPlayer.addEventListener("timeupdate", function() { //listens for any change in video time. It will run the function below.
+   for (let i = 0; i < spanParagraphs.length; i++) {
+   if (parseFloat(videoPlayer.currentTime.toFixed(2)) > parseFloat(spanParagraphs[i].getAttribute("data-start"))
+          && parseFloat(videoPlayer.currentTime.toFixed(2)) < parseFloat(spanParagraphs[i].getAttribute("data-end")))
+                {
+          spanParagraphs[i].style.color = "#b76e79";
+   }
+      else {
+           spanParagraphs[i].style.color = "black";
+   }
+   }
+ });
